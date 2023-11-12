4b10e6a9464bf4481ec96c1bb26a9c666a27c95e
mathliuyang <mathliuyang@163.com>
2023-11-13 03:03:19 +0800

    Add new model and figure files for Multi-Dimensional Neutron Diffusion simulation

---
+++ Multi-Dimensional-Neutron-Diffusion/MultiDimDiffusionEquation3_2_hardBC_7.py
@@ -0,0 +1,94 @@
+
+import deepxde as dde
+import numpy as np
+import torch
+import os
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+# 初始化参数
+k_eff = 1  # 有效增殖系数
+a = 1  # 平板的宽度
+B2 = (np.pi / a) ** 2  # 系统临界时的几何曲率
+l = 16  # 神经网络的深度
+s = 20  # 神经网络的中间层隐藏神经单元数量
+Pb = 100  # 边界权重
+Pc = 100 # 额外配置点权重
+C = 0.5  # 解析解参数
+
+
+# 定义解析解
+def phi_analytical(x):
+    return C * np.cos(x * np.pi / a)
+
+
+# 定义几何网格
+geom = dde.geometry.Interval(-a / 2, a / 2)
+
+
+# 定义微分方程
+def pde(x, phi):
+    dphi_xx = dde.grad.hessian(phi, x, i=0, j=0)
+    return dphi_xx + B2 * phi
+
+
+# # 定义边界条件
+# bc = dde.icbc.DirichletBC(geom, lambda x: 0, lambda _, on_boundary: on_boundary)
+# 扩散方程特征向量加速收敛方法验证
+observe_x = np.array([0])
+observe_phi0 = dde.icbc.PointSetBC(observe_x, np.array([0.1]))
+# 定义数据
+data = dde.data.PDE(geom, pde, [observe_phi0], num_domain=898, num_boundary=2, anchors=observe_x, solution=phi_analytical, num_test=900)
+# 定义神经网络
+layer_size = [1] + [s] * l + [1]
+activation = "tanh"
+# 网络初始值权重采用高斯分布随机采样
+initializer = "Glorot uniform"
+net = dde.nn.PFNN(layer_size, activation, initializer)
+
+# 定义硬边界条件
+def output_transform(x, y):
+    return (x + a / 2) * (x - a / 2) * y
+net.apply_output_transform(output_transform)
+# ------------------------------------------------初始模型-------------------------------------------------
+# 定义模型
+model = dde.Model(data, net)
+# 定义求解器
+model.compile("adam", lr=0.001, metrics=["l2 relative error"], loss_weights=[1, Pc])
+# 修改网络的偏置项，调整初始C值
+# for module in net.modules():
+#     if isinstance(module, torch.nn.Linear):
+#         module.bias.data = torch.tensor([1.], requires_grad=True)
+
+# 输出初始网络在 x=0 处的值
+print("Predicted value  of the initial network at x=0 : {:f}".format(model.predict(np.array([0]))[0]))
+
+# 训练模型
+model.train(epochs=1000)
+# 输出在 x=0 处的值(即 C)
+print("Predicted value at x=0: {:f}".format(model.predict(np.array([0]))[0]))
+# 确保文件夹路径存在
+output_folder = "model/算例2/model6"
+os.makedirs(output_folder, exist_ok=True)
+
+model_path = "model/算例2/model7.pth"
+# 保存模型
+torch.save(model.net.state_dict(), model_path)
+
+# ------------------------------------------------迁移学习-------------------------------------------------
+# 扩散方程特征向量加速收敛方法验证
+observe_x = np.array([0])
+observe_phi0 = dde.icbc.PointSetBC(observe_x, phi_analytical(observe_x))
+# 定义数据
+data = dde.data.PDE(geom, pde, [observe_phi0], num_domain=898, num_boundary=2, anchors=observe_x, solution=phi_analytical, num_test=900)
+# 定义模型
+loaded_model = dde.Model(data, net)
+# 加载模型的状态字典
+loaded_model.net.load_state_dict(torch.load(model_path))
+print("迁移学习: Predicted value at x=0: {:f}".format(model.predict(np.array([0]))[0]))
+# 训练模型
+loaded_model.compile("adam", lr=0.001, metrics=["l2 relative error"], loss_weights=[1, Pc])
+losshistory, train_state = loaded_model.train(epochs=500)
+print("迁移学习: Predicted value at x=0: {:f}".format(model.predict(np.array([0]))[0]))
+
+# ------------------------------------------------可视化-------------------------------------------------
---
+++ Multi-Dimensional-Neutron-Diffusion/MultiDimDiffusionEquation3_3_3_ AnalyticalSolution.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+import os
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+# 模型方程的参数
+v = 2.2e3  # m/s
+D = 0.211e-2  # m
+L_square = 2.1037e-4  # m^2
+a = 1  # m
+k_infinity = 1.0041
+
+
+# 初始条件
+def phi_0(x):
+    return np.cos(np.pi * x / a) - 0.4 * np.cos(2 * np.pi * x / a) - 0.4
+
+
+# 解析解的计算
+def phi(x, t):
+    summation = 0
+    B_n = lambda n: (2 * n - 1) * np.pi / a
+    for n in range(1, 10):  # 选择一个足够大的上限来近似无穷级数
+        l_n = L_square / (D * v * (1 + L_square * B_n(n) ** 2))
+        k_n = k_infinity / (1 + L_square * B_n(n) ** 2)
+
+        integrand = lambda x_prime: phi_0(x_prime) * np.cos(B_n(n) * x_prime)
+        integral_result, _ = quad(integrand, -a / 2, a / 2)
+        summation += (2 / a) * integral_result * np.cos(B_n(n) * x) * np.exp((k_n - 1) * t / l_n)
+
+    return summation
+
+# 创建保存图像的文件夹
+output_folder = "figure/算例3"
+os.makedirs(output_folder, exist_ok=True)
+
+# 设置中文字体
+
+# 替换为您的字体文件路径
+font_path = '/System/Library/Fonts/STHeiti Light.ttc'
+
+# 添加字体路径
+font_properties = FontProperties(fname=font_path)
+plt.rcParams['font.sans-serif'] = [font_properties.get_name()]
+plt.rcParams['axes.unicode_minus'] = False
+
+# 生成二维网格
+x_values = np.linspace(-a / 2, a / 2, 100)
+t_values = np.linspace(0, 0.01, 100)
+x_mesh, t_mesh = np.meshgrid(x_values, t_values)
+
+# 计算解在二维网格上的值
+phi_values = phi(x_mesh, t_mesh)
+
+plt.show()
+# 绘制三维图
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x_mesh, t_mesh, phi_values, cmap='viridis', linewidth=0.5,edgecolors='k')
+# ax.set_title('解析解')
+ax.set_xlabel('x (m)')
+ax.set_ylabel('t (s)')
+ax.set_zlabel('$\phi(x, t)$')
+# 自定义视角
+ax.view_init(elev=30, azim=235)  # 仰角为30度，方位角为45度
+# 添加颜色条
+# colorbar = fig.colorbar(surf, ax=ax, pad=0.1)
+# colorbar.set_label('$\phi(x, t)$', rotation=270, labelpad=15)
+
+plt.show()
+
+# 保存为png
+plt.tight_layout()
+plt.savefig(os.path.join(output_folder, "AnalyticalSolution1.png"), format="png", dpi=200)
